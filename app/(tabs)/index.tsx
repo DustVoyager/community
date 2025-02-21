80e7278ced83f17658fd4fc293b8b8fee3d7c7f8
DustVoyager <ohjamdol@gmail.com>
2025-02-21 18:18:38 +0900

    pressable buttom component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
+import CustomButton from "@/components/CustomButton";
 import { SafeAreaView, StyleSheet, Text } from "react-native";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
       <Text>홈스크린</Text>
+      <CustomButton label="버튼" onPress={() => {}} />
     </SafeAreaView>
   );
 }
