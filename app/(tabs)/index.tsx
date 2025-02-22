bfb4baf1911a623bcf2325c2fd790b5b1da4fbd1
DustVoyager <ohjamdol@gmail.com>
2025-02-22 16:44:41 +0900

    auth home markup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
 import CustomButton from "@/components/CustomButton";
+import { router } from "expo-router";
 import { SafeAreaView, StyleSheet, Text } from "react-native";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
       <Text>홈스크린</Text>
-      <CustomButton label="버튼" onPress={() => {}} />
+      <CustomButton label="버튼" onPress={() => router.push("/auth")} />
     </SafeAreaView>
   );
 }
