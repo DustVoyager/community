068fc509d017fd701c661ea89b20ede7ba7089a1
DustVoyager <ohjamdol@gmail.com>
2025-02-25 19:19:55 +0900

    secure store + api 만들기

---
+++ api/axios.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { Platform } from "react-native";
+
+//exp://192.168.219.102:8081
+
+const baseUrls = {
+  android: "http://10.0.2.2:3030",
+  ios: "http://localhost:3030",
+};
+
+const axiosInstance = axios.create({
+  baseURL: Platform.OS === "ios" ? baseUrls.ios : baseUrls.android,
+});
+
+export default axiosInstance;
