6d79c98350fa8e54a6d0b74d83d2f1653ba5b769
DustVoyager <ohjamdol@gmail.com>
2025-03-27 22:56:23 +0900

    profile folder structure update

---
+++ app/profile/[id].tsx
@@ -0,0 +1,10 @@
+import AuthRoute from "@/components/AuthRoute";
+import { View } from "react-native";
+
+export default function ProfileScreen() {
+  return (
+    <AuthRoute>
+      <View></View>
+    </AuthRoute>
+  );
+}
